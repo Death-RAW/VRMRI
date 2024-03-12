f1b9cfb7abb0b552fbf8eabb6c3c6eee59c616b1
Death-RAW <iresh88@gmail.com>
2024-03-12 16:06:38 +0200

    nurse ai ctrl added

---
+++ Assets/Scripts/AI_follow_destination.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AI_follow_destination : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public Transform player;
+    public Animator aiAnim;
+    Vector3 dest;
+
+    void Update()
+    {
+        dest = player.position;
+        ai.destination = dest;
+        if (ai.remainingDistance <= ai.stoppingDistance)
+        {
+            aiAnim.ResetTrigger("walk");
+            aiAnim.SetTrigger("idle");
+        }
+        else
+        {
+            aiAnim.ResetTrigger("idle");
+            aiAnim.SetTrigger("walk");
+        }
+    }
+}
+
+
+
