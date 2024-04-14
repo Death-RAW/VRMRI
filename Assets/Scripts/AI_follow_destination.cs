bc41482bbee543681914201dcf60028cb2b7191d
Death-RAW <iresh88@gmail.com>
2024-04-14 18:58:51 +0300

    blink and moutth anim with head tilt

--- Assets/Scripts/AI_follow_destination.cs
+++ Assets/Scripts/AI_follow_destination.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class AI_follow_destination : MonoBehaviour
 {
     public NavMeshAgent ai;
@@ -9,12 +10,29 @@ public class AI_follow_destination : MonoBehaviour
     public Animator aiAnim;
     Vector3 dest;
 
+    IEnumerator StartAIWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        enabled = true; // Enable the AI_follow_destination script
+    }
+
+    void Start()
+    {
+        enabled = false; // Disable the script initially
+        StartCoroutine(StartAIWithDelay(10f)); // Start the coroutine with a 10-second delay
+    }
+
     void Update()
     {
+        if (!enabled)
+            return; // If the script is disabled, do not execute the following code
+        
+        aiAnim.SetTrigger("walk");
         dest = player.position;
         ai.destination = dest;
         if (ai.remainingDistance <= ai.stoppingDistance)
         {
+            Debug.Log("iiiinnnn");
             aiAnim.ResetTrigger("walk");
             aiAnim.SetTrigger("idle");
         }
@@ -25,6 +43,3 @@ public class AI_follow_destination : MonoBehaviour
         }
     }
 }
-
-
-
--- Assets/Scripts/MyDoorController.cs
+++ Assets/Scripts/MyDoorController.cs
@@ -1,32 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class door_anim_trigger : MonoBehaviour
-{
+// public class door_anim_trigger : MonoBehaviour
+// {
     
-    [SerializeField] private Animator myDoor = null;
-    [SerializeField] private bool openTrigger = false;
-    [SerializeField] private bool closeTrigger = false;
+//     [SerializeField] private Animator myDoor = null;
+//     [SerializeField] private bool openTrigger = false;
+//     [SerializeField] private bool closeTrigger = false;
     
-    private void Update(){
-        myDoor.Play("Door_open", 0, 0.0f);
-    }
+//     private void Update(){
+//         myDoor.Play("Door_open", 0, 0.0f);
+//     }
 
-    private void OnTriggerEnter(Collider other){
-        Debug.Log("in");
-        if(other.CompareTag("Player")){
-            if(openTrigger){
-                Debug.Log("in");
-                myDoor.Play("Door_open", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
-            else if(closeTrigger){
-                myDoor.Play("Door_close", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
-        }
-    }
+//     private void OnTriggerEnter(Collider other){
+//         Debug.Log("in");
+//         if(other.CompareTag("Player")){
+//             if(openTrigger){
+//                 Debug.Log("in");
+//                 myDoor.Play("Door_open", 0, 0.0f);
+//                 gameObject.SetActive(false);
+//             }
+//             else if(closeTrigger){
+//                 myDoor.Play("Door_close", 0, 0.0f);
+//                 gameObject.SetActive(false);
+//             }
+//         }
+//     }
 
 	
-}
+// }
